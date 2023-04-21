2f53b08bc08ad110827ec6678387a691df25b685
ann1wu <annie.wu.1@stonybrook.edu>
2023-04-21 02:59:29 +0000

    Updated html file and python file

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
+
+some_file_path = "Template/page.html"
 app = FastAPI()
 
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def main():
+    return FileResponse(some_file_path)
+
+
+#@app.get("/")
+#async def root():
+#    return {"message": "Hello World"}
