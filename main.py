b480593ad324dfc70dfc4eac47ec6877aaaac593
ann1wu <annie.wu.1@stonybrook.edu>
2023-05-09 05:21:34 +0000

    updated data folder and python code

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 import pandas as pd
 
-df = pd.read_csv('./data/Childrenrollment.csv')
+df = pd.read_csv('./data/utilization2019.csv')
 
 app = FastAPI()
 
 @app.route('/', methods=["GET"])
 def home():
-    return 'this is a API service for NY children program enrollment code details'
+    return 'this is a API service for MN healthcare utilization code details'
 
 @app.route('/preview', methods=["GET"])
 def preview():
@@ -15,18 +15,18 @@ def preview():
     result = top10rows.to_json(orient="records")
     return result
 
-@app.route('/icd/<value>', methods=['GET'])
+@app.route('/ccd/<value>', methods=['GET'])
 def icdcode(value):
     print('value: ', value)
-    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered = df[df['country_code'] == value]
     if len(filtered) <= 0:
         return 'There is nothing here'
     else: 
         return filtered.to_json(orient="records")
 
-@app.route('/icd/<value>/sex/<value2>')
+@app.route('/ccd/<value>/sex/<value2>')
 def icdcode2(value, value2):
-    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered = df[df['country_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
