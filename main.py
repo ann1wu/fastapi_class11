c701ac5c7b1f3b35b122d495f7378431f475fbba
ann1wu <annie.wu.1@stonybrook.edu>
2023-05-09 05:26:26 +0000

    updated python code

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def preview():
     result = top10rows.to_json(orient="records")
     return result
 
-@app.route('/ccd/<value>', methods=['GET'])
-def icdcode(value):
+@app.route('/ccd/{value}', methods=['GET'])
+def countrycode(value):
     print('value: ', value)
     filtered = df[df['country_code'] == value]
     if len(filtered) <= 0:
@@ -25,7 +25,7 @@ def icdcode(value):
         return filtered.to_json(orient="records")
 
 @app.route('/ccd/<value>/sex/<value2>')
-def icdcode2(value, value2):
+def countrycode2(value, value2):
     filtered = df[df['country_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
