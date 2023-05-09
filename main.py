835f3c9f964cf918abb60b6807a7743d50165b81
ann1wu <annie.wu.1@stonybrook.edu>
2023-05-09 05:35:43 +0000

    fixed python

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ def home():
 def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
-    return result
+    return {result}
 
 @app.route('/ccd/{value}', methods=['GET'])
 def countycode(value):
     print('value: ', value)
     filtered = df[df['county_code'] == value]
     if len(filtered) <= 0:
-        return 'There is nothing here'
+        return {'There is nothing here'}
     else: 
-        return filtered.to_json(orient="records")
+        return {filtered.to_json(orient="records")}
 
-@app.route('/ccd/<value>/sex/<value2>')
+@app.route('/ccd/{value}/sex/{value2}')
 def countycode2(value, value2):
     filtered = df[df['county_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
-        return 'There is nothing here'
+        return {'There is nothing here'}
     else: 
-        return filtered2.to_json(orient="records")  
+        return {filtered2.to_json(orient="records")}
