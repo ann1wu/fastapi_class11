8b6efaf63057296980bb9f71d1f42831613eaa0a
ann1wu <annie.wu.1@stonybrook.edu>
2023-05-09 05:37:47 +0000

    fixed python

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ df = pd.read_csv('./data/utilization2019.csv')
 
 app = FastAPI()
 
-@app.route('/', methods=["GET"])
-def home():
+@app.route('/')
+async def root():
     return 'this is a API service for MN healthcare utilization code details'
 
-@app.route('/preview', methods=["GET"])
-def preview():
+@app.route('/preview')
+async def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
     return {result}
 
-@app.route('/ccd/{value}', methods=['GET'])
-def countycode(value):
+@app.route('/ccd/{value}')
+async def countycode(value):
     print('value: ', value)
     filtered = df[df['county_code'] == value]
     if len(filtered) <= 0:
@@ -25,7 +25,7 @@ def countycode(value):
         return {filtered.to_json(orient="records")}
 
 @app.route('/ccd/{value}/sex/{value2}')
-def countycode2(value, value2):
+async def countycode2(value, value2):
     filtered = df[df['county_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
